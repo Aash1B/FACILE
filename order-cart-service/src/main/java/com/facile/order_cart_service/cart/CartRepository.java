043a0bafb09d71ca8f8a2ced140af0002a230a44
Aashi <aashi222999@gmail.com>
2026-07-10 02:13:51 +0530

    Add Cart entity, repository, service, controller with basic security config

---
+++ order-cart-service/src/main/java/com/facile/order_cart_service/cart/CartRepository.java
@@ -0,0 +1,12 @@
+package com.facile.order_cart_service.cart;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends MongoRepository<Cart, String> {
+
+    Optional<Cart> findByUserId(String userId);
+}
